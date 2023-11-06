6a756dd9ced4b0a90559d9c0f81c819232362670
Daniel <owenjodaniel19@gmail.com>
2023-11-07 00:58:05 +0300

    api intergration

--- electropulse-app/src/components/admin/SellProducts.jsx
+++ electropulse-app/src/components/admin/SellProducts.jsx
@@ -1,10 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import image from '../admin/images/user-circle-solid-24.png';
 
 const SellProducts = () => {
     const loggedInUser = localStorage.getItem('loggedInUser')
 
+    const [name, setName] = useState('');
+    const [price, setPrice] = useState('');
+    const [description, setDescription] = useState('');
+    const [category, setCategory] = useState('');
+    const [brand, setBrand] = useState('');
+    const [imageUrl, setImageUrl] = useState('');
+    const [quantity, setQuantity] = useState('');
+    const [error, setError] = useState('');
+
+    const handleAddProduct = () => {
+        const Product = { name, price, description, category, brand, imageUrl, quantity };
+
+        fetch('http://127.0.0.1:5555/products', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(Product),
+        })
+            .then((response) => {
+                if (response.ok) {
+                    alert('Product added successfully');
+                } else {
+                    setError('failed to add product')
+                }
+            })
+            .catch((error) => {
+                console.error('Error', error);
+                setError('An error occured while adding product')
+            })
+    }
 
 
     return (
@@ -97,32 +128,33 @@ const SellProducts = () => {
                         <div className="modal-body">
                             <form>
                                 <div className="mb-3">
-                                    <input type="text" className="form-control1 address-fm1" id="exampleInputPassword1" placeholder='Name' />
+                                    <input type="text" className="form-control1 address-fm1" id="exampleInputPassword1" placeholder='Name' value={name} onChange={(e) => setName(e.target.value)}/>
                                 </div>
                                 <div className="mb-3">
-                                    <input type="text" className="form-control1 address-fm1" id="exampleInputPassword1" placeholder='Price' />
+                                    <input type="text" className="form-control1 address-fm1" id="exampleInputPassword1" placeholder='Price' value={price} onChange={(e) => setPrice(e.target.value)}/>
                                 </div>
                                 <div className="mb-3">
-                                    <input type="text" className="form-control1 address-fm1" id="exampleInputPassword1" placeholder='Description' />
+                                    <input type="text" className="form-control1 address-fm1" id="exampleInputPassword1" placeholder='Description' value={description} onChange={(e) => setDescription(e.target.value)}/>
                                 </div>
                                 <div className="mb-3">
-                                    <input type="text" className="form-control1 address-fm1" id="exampleInputPassword1" placeholder='category' />
+                                    <input type="text" className="form-control1 address-fm1" id="exampleInputPassword1" placeholder='category' value={category} onChange={(e) =>  setCategory(e.target.value)}/>
                                 </div>
                                 <div className="mb-3">
-                                    <input type="text" className="form-control1 address-fm1" id="exampleInputPassword1" placeholder='Brand' />
+                                    <input type="text" className="form-control1 address-fm1" id="exampleInputPassword1" placeholder='Brand' value={brand} onChange={(e) => setBrand(e.target.value)}/>
                                 </div>
                                 <div className="mb-3">
-                                    <input type="text" className="form-control1 address-fm1" id="exampleInputPassword1" placeholder='Image_url' />
+                                    <input type="text" className="form-control1 address-fm1" id="exampleInputPassword1" placeholder='Image_url' value={imageUrl} onChange={(e) => setImageUrl(e.target.value)}/>
                                 </div>
                                 <div className="mb-3">
-                                    <input type="number" className="form-control1 address-fm1" id="exampleInputPassword1" placeholder='Quantity' />
+                                    <input type="number" className="form-control1 address-fm1" id="exampleInputPassword1" placeholder='Quantity' value={quantity} onChange={(e) => setQuantity(e.target.value)}/>
                                 </div>
                                 <div className="mb-3 form-check">
                                     <input type="checkbox" className="form-check-input address-fm1 " id="exampleCheck1" />
                                     <label className="form-check-label" htmlFor="exampleCheck1">Confirm</label>
                                 </div>
-                                <button type="submit" className=" btn btn-outline-warning btn-sm my-3 fs-5">Add</button>
+                                <button type="submit" className=" btn btn-outline-warning btn-sm my-3 fs-5" onClick={handleAddProduct}>Add</button>
                             </form>
+                            {error && <p className='error-message'>{error}</p>}
                         </div>
                     </div>
                 </div>
