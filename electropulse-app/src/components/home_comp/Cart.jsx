c7dc42fa855e8a053ff98be1a34749c85c44fdf6
Daniel <owenjodaniel19@gmail.com>
2023-11-10 09:35:26 +0300

    done styling the cart view

--- electropulse-app/src/components/home_comp/Cart.jsx
+++ electropulse-app/src/components/home_comp/Cart.jsx
@@ -4,14 +4,14 @@ import { Link } from "react-router-dom";
 const Cart = ({ cartItems, handleRemove, price, handlePrice }) => {
   return (
     <div className="container">
-      <h2>Shopping Cart</h2>
+      {/* <h2>Shopping Cart</h2> */}
       {cartItems.length === 0 ? (
-        <p>Cart is empty</p>
+        <p></p>
       ) : (
         <div className="d-flex flex-wrap">
           {cartItems.map((cartItem) => (
-            <div className="col-md-4 mb-4" key={cartItem.id}>
-              <div className="card border border-success shadow p-3 mb-5 bg-body rounded">
+            <div className="col-md-4 mb-4 cart-item" key={cartItem.id}>
+              <div className="card border border-success shadow p-3 mb-5 bg-body rounded " style={{width: '7cm', height: '90%'}}>
                 <img src={cartItem.image_url} className="card-img-top" alt="Product" />
                 <div className="card-body">
                   <p className="card-text">{cartItem.brand}</p>
@@ -19,45 +19,7 @@ const Cart = ({ cartItems, handleRemove, price, handlePrice }) => {
                   <p className="card-text">Quantity: {cartItem.quantity}</p>
                   <p className="card-text">Price: Ksh {cartItem.price * cartItem.quantity}</p>
 
-                  <div className="btn-group">
-                    {/* Decrement quantity */}
-                    <button
-                      className='btn btn-outline-secondary'
-                      onClick={() => {
-                        if (cartItem.quantity > 1) {
-                          cartItem.quantity -= 1;
-                          handlePrice();
-                        }
-                      }}
-                    >
-                      -
-                    </button>
-
-                    {/* Display the quantity */}
-                    <button className='btn btn-outline-secondary'>{cartItem.quantity}</button>
-
-                    {/* Increment quantity */}
-                    <button
-                      className='btn btn-outline-secondary'
-                      onClick={() => {
-                        cartItem.quantity += 1;
-                        handlePrice();
-                      }}
-                    >
-                      +
-                    </button>
-
-                    {/* Remove item */}
-                    <button
-                      className="btn btn-outline-danger"
-                      onClick={() => {
-                        handleRemove(cartItem.id);
-                        handlePrice();
-                      }}
-                    >
-                      Remove
-                    </button>
-                  </div>
+                  
 
                   {/* Buy Now button */}
                   <Link to="/payment" target="_blank">
@@ -77,3 +39,9 @@ const Cart = ({ cartItems, handleRemove, price, handlePrice }) => {
 };
 
 export default Cart;
+
+
+
+
+// increment and decrementing
+ 
