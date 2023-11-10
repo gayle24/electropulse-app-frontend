a7f8b6a249f04fb43212929baf7dc69441c397ad
gilbert-ku <gilbert45ku@gmail.com>
2023-11-10 09:00:34 +0300

    add style

--- electropulse-app/src/components/home_comp/Cart.jsx
+++ electropulse-app/src/components/home_comp/Cart.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import { Link } from "react-router-dom";
 
 const Cart = ({ cartItems, handleRemove, price, handlePrice }) => {
@@ -8,76 +8,72 @@ const Cart = ({ cartItems, handleRemove, price, handlePrice }) => {
       {cartItems.length === 0 ? (
         <p>Cart is empty</p>
       ) : (
-        <div className="d-flex flex-wrap ">
+        <div className="d-flex flex-wrap">
           {cartItems.map((cartItem) => (
-            <div className="col" key={cartItem.id}>
+            <div className="col-md-4 mb-4" key={cartItem.id}>
               <div className="card border border-success shadow p-3 mb-5 bg-body rounded">
-                <img src={cartItem.image_url} className="card-img-top" alt="..." />
+                <img src={cartItem.image_url} className="card-img-top" alt="Product" />
                 <div className="card-body">
                   <p className="card-text">{cartItem.brand}</p>
                   <p className="card-title">{cartItem.name}</p>
                   <p className="card-text">Quantity: {cartItem.quantity}</p>
                   <p className="card-text">Price: Ksh {cartItem.price * cartItem.quantity}</p>
 
-                  {/*  Update total price after removing item */}
-                  <button
-                    type="button"
-                    className="btn btn-outline-danger"
-                    onClick={() => {
-                      handleRemove(cartItem.id);
-                      handlePrice(); 
-                    }}
-                  >
-                    Remove
-                  </button>
-                  <div>
-              
-                    {/* Update total price after decrementing quantity */}
+                  <div className="btn-group">
+                    {/* Decrement quantity */}
                     <button
-                    className='w-25'
+                      className='btn btn-outline-secondary'
                       onClick={() => {
-                        cartItem.quantity -= 1;
-                        handlePrice(); 
+                        if (cartItem.quantity > 1) {
+                          cartItem.quantity -= 1;
+                          handlePrice();
+                        }
                       }}
                     >
                       -
                     </button>
 
-                    {/* dislpaly the quantity */}
-                    <button className='w-25'>{cartItem.quantity}</button>
+                    {/* Display the quantity */}
+                    <button className='btn btn-outline-secondary'>{cartItem.quantity}</button>
 
-                     {/*  Update total price after incrementing quantity */}
+                    {/* Increment quantity */}
                     <button
-                    className='w-25'
+                      className='btn btn-outline-secondary'
                       onClick={() => {
                         cartItem.quantity += 1;
-                        handlePrice(); 
+                        handlePrice();
                       }}
                     >
                       +
                     </button>
-                    <Link to="/payment" target="_blank">
-                      <button className='bg-warning w-25'>Buy Now</button>
-                    </Link>
+
+                    {/* Remove item */}
+                    <button
+                      className="btn btn-outline-danger"
+                      onClick={() => {
+                        handleRemove(cartItem.id);
+                        handlePrice();
+                      }}
+                    >
+                      Remove
+                    </button>
                   </div>
+
+                  {/* Buy Now button */}
+                  <Link to="/payment" target="_blank">
+                    <button className='btn btn-warning mt-2 w-100'>Buy Now</button>
+                  </Link>
                 </div>
               </div>
             </div>
           ))}
-          <div>
+          <div className="col-12">
             <h4>Total Price: Ksh {price}</h4>
           </div>
         </div>
       )}
-
-
     </div>
-
-
   );
 };
 
 export default Cart;
-
-
-
